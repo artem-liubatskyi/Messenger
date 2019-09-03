80a5da1adc4f4a799ea40a577d8b99aff45a4658
Artem Liubatskyi <deadly241@gmail.com>
2019-09-04 02:14:35 +0300

    Implemented MessageService

--- Messenger.DataAccess/Interfaces/IMessageRepository.cs
+++ Messenger.DataAccess/Interfaces/IMessageRepository.cs
@@ -6,10 +6,10 @@ namespace Messenger.DataAccess.Interfaces
 {
     public interface IMessageRepository
     {
-        Message Add(Message entity);
-        Message Get(string messageId);
-        Message Update(Message entity);
-        void Delete(string messageId);
+        Task<Message> Add(Message entity);
+        Task<Message> Get(string messageId);
+        Task<Message> Update(Message entity);
+        void Delete(Message entity);
         Task<IEnumerable<Message>> GetUserMessagesByChatId(string userId, string chatId);
         void Delete(IReadOnlyCollection<Message> messages);
     }
---
+++ Messenger.Services/MessageService.cs
@@ -0,0 +1,45 @@
+﻿using Messenger.Data.Entities;
+using Messenger.DataAccess.Interfaces;
+using Messenger.Services.Defenitions;
+using Messenger.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly IMessageRepository messageRepository;
+        public async Task<ServiceResult<Message>> Add(Message message)
+        {
+            var messageEntity = await messageRepository.Add(message);
+            return new ServiceResult<Message>(messageEntity);
+        }
+
+        public async Task<ServiceResult<Message>> Delete(string messageId)
+        {
+            var messageEntity = await messageRepository.Get(messageId);
+
+            if (messageEntity == null)
+                return new ServiceResult<Message>($"No message with id {messageId}");
+
+            messageRepository.Delete(messageEntity);
+
+            return new ServiceResult<Message>(messageEntity);
+        }
+
+        public async Task<ServiceResult<Message>> Edit(Message message)
+        {
+            var messageEntity = await messageRepository.Get(message.Id.ToString());
+
+            if (messageEntity == null)
+                return new ServiceResult<Message>($"No message with id {message.Id.ToString()}");
+
+            messageEntity = await messageRepository.Update(message);
+
+            return new ServiceResult<Message>(messageEntity);
+        }
+    }
+}
