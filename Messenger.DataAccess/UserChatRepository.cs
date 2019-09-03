45849a5ea4cec69f975806705e5c02761896ab3a
Artem Liubatskyi <deadly241@gmail.com>
2019-09-04 02:33:08 +0300

    Added IUserChatRepository implementation

---
+++ Messenger.DataAccess/UserChatRepository.cs
@@ -0,0 +1,39 @@
+﻿using Messenger.Data;
+using Messenger.Data.Entities;
+using Messenger.DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messenger.DataAccess
+{
+    public class UserChatRepository : IUserChatRepository
+    {
+        private readonly MessengerDbContext context;
+
+        public UserChatRepository(MessengerDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<UserChat> Add(UserChat entity)
+        {
+            var result = await context.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return result.Entity;
+        }
+
+        public void Delete(UserChat entity)
+        {
+            context.UserChats.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public async Task<UserChat> Get(string userId, string chatId)
+        {
+            return await context.UserChats
+                .Where(x => x.UserId == userId && x.ChatId.ToString() == chatId)
+                .FirstOrDefaultAsync();
+        }
+    }
+}
